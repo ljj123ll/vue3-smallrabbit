22c0a2f27f069b06f2a6dcf869a6ad41a6ce6726
LuoMan <107547031+ljj123ll@users.noreply.github.com>
2025-09-10 11:23:38 +0800

    Add user navigation and logout confirmation
    Replaced placeholder content with a navigation bar that displays user info, logout confirmation, and relevant links. Integrated user store and router for logout logic and conditional rendering based on authentication state.

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,13 +1,60 @@
-<template>
-    <div>
-        <h1>nihao</h1>
-    </div>
-</template>
-
-
 <script setup>
+import { useUserStore } from '@/stores'
+import { useRouter } from 'vue-router'
 
+const userStore = useUserStore()
+const router = useRouter()
+
+// 定义一个confirm函数处理退出登录的确认逻辑
+const confirm = () => {
+    // 清除用户信息,store中的方法
+    userStore.clearUserInfo()
+    // 跳转到登录页面
+    router.push('/login')
+}
 </script>
+
+
+<template>
+    <nav class="app-topnav">
+        <div class="container">
+            <ul>
+                <template v-if="userStore.userInfo.token">
+                    <li>
+                        <a href="javascript:;">
+                            <i class="iconfont icon-user"></i>
+                            {{ userStore.userInfo.username }}
+                        </a>
+                    </li>
+                    <li>
+                        <el-popconfirm
+                            title="确认退出登录吗？"
+                            confirm-button-text="确认"
+                            cancel-button-text="取消"
+                            @confirm="confirm"
+                        >
+                        <!-- popconfirm显示什么？ -->
+                        <template #reference>
+                            <a href="javascript:;">退出登录</a>
+                        </template>
+                        </el-popconfirm>
+                    </li>
+                    <li><a href="javascript:;">我的订单</a></li>
+                    <li>
+                        <RouterLink :to="'/login'">登录</RouterLink>
+                    </li>
+                </template>
+                <template v-else>
+                    <li>
+                        <RouterLink :to="'/login'">登录</RouterLink>
+                    </li>
+                    <li><a href="javascript:;">帮助中心</a></li>
+                    <li><a href="javascript:;">关于我们</a></li>
+                </template>
+            </ul>
+        </div>
+    </nav>
+</template>
 
 
 <style scoped lang="scss">
